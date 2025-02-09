797c4b8931a5921958e5b0dfa9fe7147a6a4498a
dazkool <nvkulkarni.1108@gmail.com>
2025-02-09 15:04:39 +0530

    fix styles for contact form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,11 +2,10 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div>
+    <div className="contact-container">
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLSer7cxXJhI_FoY_6y_eOpTCLweVnNY2gMbtvzNcnhmyDikctg/viewform?embedded=true"
-        width="100%"
-        height="100%"
+        className="contact-form"
         allowFullScreen
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"

c94b659dc893fb260861d52a93f1c04fa72bf6ff
dazkool <nvkulkarni.1108@gmail.com>
2025-02-09 19:11:07 +0530

    SEO optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,20 +2,35 @@ import type { Metadata } from "next";
 import "@/app/ui/globals.css";
 import { inter } from "@/app/ui/fonts";
 import { FadeInSection } from "@/components/animations/fade-in";
+import Head from "next/head";
 
 export const metadata: Metadata = {
-  title: "Morya Corp Services",
-  description: "Your all-in-one Payroll Solutions in one place",
+  title: "Morya Corp Facility",
+  description:
+    "Morya Corp Services - Providing top-notch services for your business needs.",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  other: {
+    "application-ld+json": JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "Organization",
+      name: "Morya Corp Facility",
+      url: "https://www.moryacorp.in",
+      logo: "https://www.moryacorp.in/logo.png",
+    }),
+  },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </>
   );
 }

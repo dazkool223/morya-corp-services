ce17bb72c2cfb27b814c2e99bc7a39bcb63a0573
dazkool <nvkulkarni.1108@gmail.com>
2025-02-09 20:06:52 +0530

    remove bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-import bundleAnalyzer from "@next/bundle-analyzer";
 const nextConfig = {
   basePath: "/morya-corp-services",
 };
 
-const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-});
-
-export default withBundleAnalyzer(nextConfig);
+export default nextConfig;

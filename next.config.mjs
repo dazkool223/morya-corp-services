3960a3faadc854426d457885082b21b19884165b
dazkool <nvkulkarni.1108@gmail.com>
2025-02-09 19:44:21 +0530

    optimized imports and packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+import bundleAnalyzer from "@next/bundle-analyzer";
 const nextConfig = {
   basePath: "/morya-corp-services",
 };
 
-export default nextConfig;
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+export default withBundleAnalyzer(nextConfig);
